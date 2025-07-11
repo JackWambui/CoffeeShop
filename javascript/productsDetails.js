5cbd7b8b4323a1ee6ce1d8b3940835198cd8a433
JackWambui <146149968+JackWambui@users.noreply.github.com>
2025-07-11 13:37:52 +1000

    Update productsDetails.js

--- javascript/productsDetails.js
+++ javascript/productsDetails.js
@@ -1,112 +1,112 @@
-const products = [
-    {
-        id:1,
-        name:"Americano",
-        image:"https://images.unsplash.com/photo-1551030173-122aabc4489c?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW1lcmljYW5vfGVufDB8fDB8fHww",
-        category:1,
-        price:"4.30",
-        ratings:{
-            count:600,
-            stars:4
-        }
-    },
-    {
-        id:2,
-        name:"Brewed Coffee",
-        image:"https://images.unsplash.com/photo-1638202525812-53881ca0f4b5?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fGJyZXdlZCUyMGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D",
-        category:1,
-        price:"4.00",
-        ratings:{
-            count:1200,
-            stars:4.5
-        }
-    },
-    {
-        id:3,
-        name:"Caffe Latte",
-        image:"https://plus.unsplash.com/premium_photo-1670445287762-372300cdcb77?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZmUlMjBsYXR0ZXxlbnwwfHwwfHx8MA%3D%3D",
-        category:1,
-        price:"5.00",
-        ratings:{
-            count:1600,
-            stars:4.8
-        }
-    },
-    {
-        id:4,
-        name:"Caffe Mocha",
-        image:"https://images.unsplash.com/photo-1650486280607-93d026bd5213?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGNhZmZlJTIwbW9jaGF8ZW58MHx8MHx8fDA%3D",
-        category:1,
-        price:"3.60",
-        ratings:{
-            count:600,
-            stars:4.6
-        }
-    },
-    // {
-    //     id:5,
-    //     name:"Cappuccino",
-    //     image:"https://images.unsplash.com/photo-1534687941688-651ccaafbff8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FwcHVjY2lub3xlbnwwfHwwfHx8MA%3D%3D",
-    //     category:1,
-    //     price:"4.00",
-    //     ratings:{
-    //         count:300,
-    //         stars:4.2
-    //     }
-    // },
-    // {
-    //     id:6,
-    //     name:"Expresso",
-    //     image:"https://images.unsplash.com/photo-1596952954288-16862d37405b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXhwcmVzc298ZW58MHx8MHx8fDA%3D",
-    //     category:1,
-    //     price:"7.00",
-    //     ratings:{
-    //         count:1200,
-    //         stars:4.8
-    //     }
-    // },
-    {
-        id:7,
-        name:"Chocolate Cake",
-        image:"https://plus.unsplash.com/premium_photo-1715015440855-7d95cf92608a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvY29sYXRlJTIwY2FrZXxlbnwwfHwwfHx8MA%3D%3D",
-        category:2,
-        price:"6.99",
-        ratings:{
-            count:2000,
-            stars:4.7
-        }
-    },
-    {
-        id:8,
-        name:"Red Velvet Cake",
-        image:"https://plus.unsplash.com/premium_photo-1713920189815-876dbdf5f56e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8UmVkJTIwVmVsdmV0JTIwQ2FrZXxlbnwwfHwwfHx8MA%3D%3D",
-        category:2,
-        price:"6.99",
-        ratings:{
-            count:2400,
-            stars:4.75
-        }
-    },
-    {
-        id:9,
-        name:"Vanilla Cake",
-        image:"https://plus.unsplash.com/premium_photo-1663839331055-d0c9bcb2929f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8VmFuaWxsYSUyMENha2V8ZW58MHx8MHx8fDA%3D",
-        category:2,
-        price:"4.50",
-        ratings:{
-            count:200,
-            stars:4.4
-        }
-    },
-    {
-        id:10,
-        name:"Black Forest Cake",
-        image:"https://plus.unsplash.com/premium_photo-1715178982048-d36eba937bc2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8QmxhY2slMjBGb3Jlc3QlMjBDYWtlfGVufDB8fDB8fHww",
-        category:2,
-        price:"5.99",
-        ratings:{
-            count:400,
-            stars:4.6
-        }
-    }
-];
+// const products = [
+//     {
+//         id:1,
+//         name:"Americano",
+//         image:"https://images.unsplash.com/photo-1551030173-122aabc4489c?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW1lcmljYW5vfGVufDB8fDB8fHww",
+//         category:1,
+//         price:"4.30",
+//         ratings:{
+//             count:600,
+//             stars:4
+//         }
+//     },
+//     {
+//         id:2,
+//         name:"Brewed Coffee",
+//         image:"https://images.unsplash.com/photo-1638202525812-53881ca0f4b5?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fGJyZXdlZCUyMGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D",
+//         category:1,
+//         price:"4.00",
+//         ratings:{
+//             count:1200,
+//             stars:4.5
+//         }
+//     },
+//     {
+//         id:3,
+//         name:"Caffe Latte",
+//         image:"https://plus.unsplash.com/premium_photo-1670445287762-372300cdcb77?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZmUlMjBsYXR0ZXxlbnwwfHwwfHx8MA%3D%3D",
+//         category:1,
+//         price:"5.00",
+//         ratings:{
+//             count:1600,
+//             stars:4.8
+//         }
+//     },
+//     {
+//         id:4,
+//         name:"Caffe Mocha",
+//         image:"https://images.unsplash.com/photo-1650486280607-93d026bd5213?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGNhZmZlJTIwbW9jaGF8ZW58MHx8MHx8fDA%3D",
+//         category:1,
+//         price:"3.60",
+//         ratings:{
+//             count:600,
+//             stars:4.6
+//         }
+//     },
+//     // {
+//     //     id:5,
+//     //     name:"Cappuccino",
+//     //     image:"https://images.unsplash.com/photo-1534687941688-651ccaafbff8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FwcHVjY2lub3xlbnwwfHwwfHx8MA%3D%3D",
+//     //     category:1,
+//     //     price:"4.00",
+//     //     ratings:{
+//     //         count:300,
+//     //         stars:4.2
+//     //     }
+//     // },
+//     // {
+//     //     id:6,
+//     //     name:"Expresso",
+//     //     image:"https://images.unsplash.com/photo-1596952954288-16862d37405b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXhwcmVzc298ZW58MHx8MHx8fDA%3D",
+//     //     category:1,
+//     //     price:"7.00",
+//     //     ratings:{
+//     //         count:1200,
+//     //         stars:4.8
+//     //     }
+//     // },
+//     {
+//         id:7,
+//         name:"Chocolate Cake",
+//         image:"https://plus.unsplash.com/premium_photo-1715015440855-7d95cf92608a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvY29sYXRlJTIwY2FrZXxlbnwwfHwwfHx8MA%3D%3D",
+//         category:2,
+//         price:"6.99",
+//         ratings:{
+//             count:2000,
+//             stars:4.7
+//         }
+//     },
+//     {
+//         id:8,
+//         name:"Red Velvet Cake",
+//         image:"https://plus.unsplash.com/premium_photo-1713920189815-876dbdf5f56e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8UmVkJTIwVmVsdmV0JTIwQ2FrZXxlbnwwfHwwfHx8MA%3D%3D",
+//         category:2,
+//         price:"6.99",
+//         ratings:{
+//             count:2400,
+//             stars:4.75
+//         }
+//     },
+//     {
+//         id:9,
+//         name:"Vanilla Cake",
+//         image:"https://plus.unsplash.com/premium_photo-1663839331055-d0c9bcb2929f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8VmFuaWxsYSUyMENha2V8ZW58MHx8MHx8fDA%3D",
+//         category:2,
+//         price:"4.50",
+//         ratings:{
+//             count:200,
+//             stars:4.4
+//         }
+//     },
+//     {
+//         id:10,
+//         name:"Black Forest Cake",
+//         image:"https://plus.unsplash.com/premium_photo-1715178982048-d36eba937bc2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8QmxhY2slMjBGb3Jlc3QlMjBDYWtlfGVufDB8fDB8fHww",
+//         category:2,
+//         price:"5.99",
+//         ratings:{
+//             count:400,
+//             stars:4.6
+//         }
+//     }
+// ];
