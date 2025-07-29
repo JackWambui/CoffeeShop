8932a23eee3becf3043891ea81c58c10eb82a6eb
Nedup2019 <nedupdorji89@gmail.com>
2025-07-29 18:54:32 +1000

    Update fetchEmployeesDetails.php

--- php/fetchEmployeesDetails.php
+++ php/fetchEmployeesDetails.php
@@ -37,51 +37,12 @@
         }else{
             echo "no data is available";
         }
-        $productsInformation = json_encode($productsArray);
-        echo $productsInformation;
+        $employeesInformation = json_encode($employeesArray );
+        echo $employeesInformation;
     $connection->close();
 ?>
 
 
 
 
-  // $message = "see me";
-        $preparedstmt1 = "SELECT * FROM employees";
-        $preparedstmt1Result = $connection->query($preparedstmt1);
-        $ordersDetailsFromDB = array();
-
-        if($preparedstmt1Result->num_rows > 0){
-            // $message = "Data is available";
-           
-            while($rows = $preparedstmt1Result->fetch_assoc()){
-                $empIDDB = $rows["emp_Id"];
-                $empfirstnameDB = $rows["firstname"];
-                $emplastnameDB = $rows["lastname"];
-                $empemailDB = $rows["email"];
-                $emppositionDB = $rows["position"];
-                $empStart_dateDB = $rows["Start_date"];
-                $empcreated_atDB = $rows["created_at"];
-                $empupdatedDB = $rows["updated"];
-
-                 $EmpDetailFromDB = array(
-                    "emp_Id"     => (int)$empIDDB,
-                    "empfirstname" =>$empfirstnameDB,
-                    "emplastname" =>$emplastnameDB,
-                    "empemail" =>$empemailDB,
-                    "empposition" =>$emppositionDB,
-                    "empStart_date" =>$empStart_dateDB,  
-                );
-             
-
-                // $orderDetailFromDB = array(
-                //     "orderID"     => (int)$orderIDDB,
-                //     "orderNumber" => $orderNumberDB,
-                //     "orderStatus" => ucfirst($orderStatusDB),
-                //     "createdAt"   => $createdAtDB
-                // );
-
-                array_push($ordersDetailsFromDB,$orderDetailFromDB);
-            }
-            $message = $ordersDetailsFromDB;
-        }else{
-            
+ 
